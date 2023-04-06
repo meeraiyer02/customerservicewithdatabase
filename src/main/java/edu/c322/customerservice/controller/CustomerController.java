a285c53cee52b8832289e484deec8a42b8144245
meeraiyer <meeriyer@iu.edu>
2023-04-06 15:23:35 -0400

    Added postgres db

---
+++ src/main/java/edu/c322/customerservice/controller/CustomerController.java
@@ -0,0 +1,59 @@
+package edu.c322.customerservice.controller;
+
+import java.util.List;
+
+import edu.c322.customerservice.repository.CustomerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.c322.customerservice.model.Customer;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private CustomerRepository repository;
+
+    public CustomerController(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    // Get https:localhost:8080/customers
+    @GetMapping
+    public List<Customer> findAll(){
+        return repository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public int create(@Valid @RequestBody Customer customer){
+        Customer addedCustomer = repository.save(customer);
+        return addedCustomer.getId();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@Valid @RequestBody Customer customer, @PathVariable int id){
+        customer.setId(id);
+        repository.save(customer);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        Customer customer = new Customer();
+        customer.setId(id);
+        repository.delete(customer);
+    }
+   
+    
+}
